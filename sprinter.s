da76205d6d6388dca3c2861293344f10f0fb2c3e
Mvrcus97 <marcus-tierney@hotmail.com>
2018-04-24 23:45:13 +0200

    Add files via upload

--- sprinter.s
+++ sprinter.s
@@ -204,7 +204,8 @@ sprinter:
 		incl %ecx          # Increase counter.
 
 		movb $120, (%ebx)   # Copy 'x' to result
-		# No need to increase %EBX and %ECX. It's done later ->
+		incl %ebx           # Point to next in result
+		# No need to increase counter, done later. ->
 		jmp hex
 
 
